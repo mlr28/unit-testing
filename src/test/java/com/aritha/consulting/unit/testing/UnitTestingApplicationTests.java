e22e7f3c2a484e4bab06240fadbc1fa5875fa918
Rohit M L <112368030+mlr28@users.noreply.github.com>
2022-11-02 17:36:49 +0530

    After Merge conflict

---
+++ src/test/java/com/aritha/consulting/unit/testing/UnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aritha.consulting.unit.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
